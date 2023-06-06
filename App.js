4028ade8d5e6c2053d08e6a39ff409cefd0feca4
hywenklis <hywenklis@hotmail.com>
2023-06-06 00:56:03 -0300

    Inicio do projeto, adicionando integração com firebase, adicionando tela de login, tela de cadastro e tela inicial

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebaseconfig'
+
+// pages
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+// import AddScreen from './screens/AddScreen'
+// import UpdateScreen from './screens/UpdateScreen'
+// import AllTransactions from './screens/AllTransactions'
+
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const app = initializeApp(firebaseConfig);
+
+  const globalScreenOptions = {
+    headerStyle: {
+      backgroundColor: '#DDD5F1',
+      // backgroundColor: '#51A3B1',
+    },
+    headerTitleStyle: {
+      color: '#000000',
+    },
+    headerTintColor: 'black',
+  }
+  return (
+    <NavigationContainer>
+      <StatusBar style='dark' />
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        {/* <Stack.Screen name='Add' component={AddScreen} />
+        <Stack.Screen name='Update' component={UpdateScreen} />
+        <Stack.Screen name='All' component={AllTransactions} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
